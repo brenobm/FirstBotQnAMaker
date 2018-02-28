a9387a15bb06691adada03862616149002283534
Breno Batista Machado <breno.machado@montreal.com.br>
2018-02-28 19:51:00 -0300

    First chatbot with QnA Maker, with HeroCards

--- FirstChatBot/Controllers/MessagesController.cs
+++ FirstChatBot/Controllers/MessagesController.cs
@@ -18,7 +18,7 @@ namespace FirstChatBot
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                await Conversation.SendAsync(activity, () => new Dialogs.QnaDialog());
             }
             else
             {
